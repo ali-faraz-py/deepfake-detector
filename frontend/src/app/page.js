23f9a6b3f2858f9693b278e9a3697d01fbc0ce75
Syed Ali Faraz <alii.exe3@gmail.com>
2026-07-09 09:28:46 +0500

    feat: update API endpoint to use environment variable for prediction requests

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
       formData.append("file", file);
 
       console.log("Sending request to backend...");
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/predict`, {
         method: "POST",
         body: formData,
       });
