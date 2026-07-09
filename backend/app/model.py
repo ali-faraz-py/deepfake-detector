e879d23094322e2aa8e2742b5c6c1710e8f1ce87
Syed Ali Faraz <alii.exe3@gmail.com>
2026-07-09 08:30:44 +0500

    implement image prediction function using EfficientNet model

--- backend/app/model.py
+++ backend/app/model.py
@@ -9,3 +9,25 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = EfficientNet.from_pretrained('ntnet-b0', num_classes=2)
 model = model.to(device)
 model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+def predict_image(image_bytes: bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    image = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(image)
+        probabilities = torch.softmax(outputs, dim=1)
+        confidence, predicted_class = torch.max(probabilities, dim=1)
+
+    label = "Fake" if predicted_class.item() == 1 else "Real"
+
+    return{
+        "prediction": label,
+        "confidence": round(confidence.item() * 100, 2)
+    }
